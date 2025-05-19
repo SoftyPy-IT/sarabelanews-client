3c20def0878a8a439bafda8131edacf5759d97ae
AlMamunTalukder <almamuntalukder0@gmail.com>
2025-05-19 17:05:42 +0600

    up

--- src/components/LeadNews/LeadNewsCard.tsx
+++ src/components/LeadNews/LeadNewsCard.tsx
@@ -40,10 +40,12 @@ const LeadNewsCard = ({ newsData }: NewsProps) => {
               {news.newsTitle }
             </h2>
             <p className="hidden md:block text-[17px] mt-2 text-gray-500">  {news?.description ? parse(truncateText(news.description, 120)) : ""}</p>
-            <h2 className="md:hidden font-bold  text-gray-700 hover:text-blue-600 transition-colors">
+            
+            
+            <h2 className="md:hidden font-bold  text-gray-700 dark:text-gray-200 transition-colors">
               {news?.newsTitle ? parse(truncateText(news.newsTitle, 35)) : ""}
             </h2>
-            <p className="md:hidden text-sm mt-2 text-gray-500">  {news?.description ? parse(truncateText(news.description, 85)) : ""}</p>
+            <p className="md:hidden text-[15px] mt-2 text-gray-500">  {news?.description ? parse(truncateText(news.description, 85)) : ""}</p>
           </div>
         </Link>
       ))}
