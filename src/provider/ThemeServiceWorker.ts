684bed9c93beab4df3ebb1b67dc45aead4f3f165
Ibrahi Sikder <ibrahimsikder5033@gmail.com>
2025-06-21 11:31:26 +0600

    merge

--- src/provider/ThemeServiceWorker.ts
+++ src/provider/ThemeServiceWorker.ts
@@ -17,8 +17,8 @@ export default function ThemeAndServiceWorker() {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker
         .register("/sw.js")
-        .then((registration) => console.log("Service Worker registered:", registration))
-        .catch((error) => console.error("Service Worker registration failed:", error));
+        // .then((registration) => console.log("Service Worker registered:", registration))
+        // .catch((error) => console.error("Service Worker registration failed:", error));
     }
   }, []);
 
